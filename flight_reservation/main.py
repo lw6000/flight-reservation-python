844d936e7e7575fa1d95510e5f2df88881c4c7a7
lwalsh6 <lwalsh6@ut.utm.edu>
2025-05-14 14:45:09 -0500

    main.py has rudimentary menu

--- flight_reservation/main.py
+++ flight_reservation/main.py
@@ -13,3 +13,21 @@ def main():
 
 if __name__ == "__main__":
     main()
+    print("Welcome to the Airline Database Program!")
+    print("Please Select an Option: ")
+    print("1 - Login ")
+    print("2 - Register ")
+    print("3 - Exit ")
+    choice = None
+    while choice != '3':
+        choice = input("Make your selection now: ")
+        if choice == '1':
+            print("Temporary Login")
+        elif choice == '2':
+            print("Temporary register")
+        elif choice == '3':
+            print("Goodbye, and thank you for using the Airline Database Program!")
+        else:
+            print(choice, "is not an option, please select options 1, 2, or 3.")
+    
+    
